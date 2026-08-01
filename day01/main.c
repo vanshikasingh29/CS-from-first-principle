3767ab8083637f2469833c3eb31435e8cda0c743
vanshikasingh29 <138687668+vanshikasingh29@users.noreply.github.com>
2026-08-01 20:48:59 +0000

    edited code

--- day01/main.c
+++ day01/main.c
@@ -9,13 +9,13 @@ int main() {
 
     printf("=== DAY 1 SYSTEM OVERVIEW ===\n");
 
-    printf("1. Value: %d\n", my_number);
+    printf("1. The value inside my_number box is: %d\n", my_number);
 
-    printf("2. Address: %p\n", (void*)&my_number);
+    printf("2. The physical RAM hardware address of my_number is: %p\n", (void*)&my_number);
 
-    printf("3. Pointer stores: %p\n", (void*)my_pointer);
+    printf("3. The address stored inside my_pointer box is: %p\n", (void*)my_pointer);
 
-    printf("4. Pointer gives value: %d\n", *my_pointer);
+    printf("4. The value sitting at the address my_pointer points to is: %d\n", *my_pointer);
 
 
     *my_pointer = 999;
@@ -23,7 +23,7 @@ int main() {
 
     printf("\n=== AFTER MEMORY CHANGE ===\n");
 
-    printf("5. New value: %d\n", my_number);
+    printf("5. The value inside my_number is now magically: %d\n", my_number);
 
 
     return 0;
